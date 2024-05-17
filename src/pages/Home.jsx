d44dc1e387f5509f979d49eb665b3832f32abbf2
SardorQobilov <sardorqobilov555@gmail.com>
2024-05-17 12:15:10 +0500

    modal bugs fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -142,12 +142,18 @@ export const Home = () => {
   // eslint-disable-next-line no-unused-vars
   const [values, setValue] = useState("");
   const [showRegionModal, setShowRegionModal] = useState(false);
+  const [placeholders, setPlaceholders] = useState(Array(10).fill("•"));
+  const [carNumber] = useState(Array(10).fill(""));
+  const [placeholders2, setPlaceholders2] = useState(Array(3).fill("•"));
+  const [carNumber2] = useState(Array(3).fill(""));
 
   const handleShowRegionModal = () => {
     setShowRegionModal(!showRegionModal);
+    document.body.classList.toggle("no__scroll");
   };
   const handleShow = () => {
     setShowDropDown(!showDropDown);
+    document.body.classList.toggle('no__scroll')
   };
   const handleInputChange = (index, event) => {
     const { value } = event.target;
@@ -167,21 +173,61 @@ export const Home = () => {
     }
 
     event.target.value = newValue;
-
     if ((index === 0 || index === 4 || index === 5) && newValue.length === 1) {
-      if (index < inputs.current.length - 1) {
+      if (index < inputs.current.length - 1 && inputs.current[index + 1]) {
         inputs.current[index + 1].focus();
       }
     }
+    if (
+      event.nativeEvent.inputType === "deleteContentBackward" &&
+      newValue === ""
+    ) {
+      if (index > 0 && inputs.current[index - 1]) {
+        inputs.current[index - 1].focus();
+        // const newPlaceholders = [...placeholders];
+        // newPlaceholders[index] = ".";
+        // setPlaceholders(newPlaceholders);
+        // const newPlaceholders2 = [...placeholders2];
+        // newPlaceholders2[index] = ".";
+        // setPlaceholders2(newPlaceholders2);
+      }
+    }
+  };
+
+  const handleFocus = (index) => {
+    const newPlaceholders = [...placeholders];
+    newPlaceholders[index] = "";
+    setPlaceholders(newPlaceholders);
+  };
+  const handleBlur = (index) => {
+    const newPlaceholders = [...placeholders];
+    if (carNumber[index] === "") {
+      newPlaceholders[index] = "•";
+    }
+    setPlaceholders(newPlaceholders);
+  };
+  const handleFocus2 = (index) => {
+    const newPlaceholders2 = [...placeholders2];
+    newPlaceholders2[index] = "";
+    setPlaceholders2(newPlaceholders2);
+  };
+  const handleBlur2 = (index) => {
+    const newPlaceholders3 = [...placeholders2];
+    if (carNumber2[index] === "") {
+      newPlaceholders3[index] = "•";
+    }
+    setPlaceholders2(newPlaceholders3);
   };
 
   return (
     <>
       <div
-        className={`overlay ${
-          showDropDown ? "show" : "" || showRegionModal ? "show" : ""
-        }`}
+        className={`overlay ${showRegionModal ? "show" : ""}`}
         onClick={handleShowRegionModal}
+      ></div>
+      <div
+        className={`overlay__two ${showDropDown ? "show" : ""}`}
+        onClick={handleShow}
       ></div>
       <div className="number__box">
         <h1 className="title">
@@ -190,15 +236,19 @@ export const Home = () => {
         <div className="number__filter-boxes">
           <div className="number__filter">
             {[...Array(6)].map((_, index) => (
-              <input
-                key={index}
-                ref={(el) => (inputs.current[index] = el)}
-                type="text"
-                className="number__text"
-                placeholder="."
-                maxLength={1}
-                onChange={(event) => handleInputChange(index, event)}
-              />
+              <label className="input__label" key={index}>
+                <input
+                  // key={index}
+                  ref={(el) => (inputs.current[index] = el)}
+                  type="text"
+                  className="number__text"
+                  placeholder={placeholders[index]}
+                  maxLength={1}
+                  onChange={(event) => handleInputChange(index, event)}
+                  onFocus={() => handleFocus(index)}
+                  onBlur={() => handleBlur(index)}
+                />
+              </label>
             ))}
           </div>
           <div className="number__group">
@@ -208,9 +258,11 @@ export const Home = () => {
                 ref={(el) => (inputs.current[index + 6] = el)}
                 type="text"
                 className="number__text"
-                placeholder="."
+                placeholder={placeholders2[index]}
                 maxLength={1}
                 onChange={(event) => handleInputChange(index + 6, event)}
+                onFocus={() => handleFocus2(index)}
+                onBlur={() => handleBlur2(index)}
               />
             ))}
           </div>
